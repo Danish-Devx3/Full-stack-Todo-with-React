3fa6d1f6cd94b9bad2ae01ccb7c5e1a39e10b5f9
Danish-Devx3 <danish.devx3@gmail.com>
2024-11-24 12:51:05 +0530

    Create project structure

---
+++ backend/index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express();
+
+app.use(express,express.json())
+app.post('/todo', function(req, res){
+
+})
+
+app.get('/todos', function(){
+
+})
+
+app.put('/completed', function(){
+    
+})
