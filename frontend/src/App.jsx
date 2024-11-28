98d8f240851aaa4901b0340932532ea4d152fc94
Danish-Devx3 <danish.devx3@gmail.com>
2024-11-28 15:57:53 +0530

    Learned how to handle input and buttons

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,23 +7,34 @@ import { Todos } from './components/Todos'
 
 function App() {
   const [todos, setTodos] = useState([]);
-
-  useEffect(() => {
-    fetch('http://localhost:3000/todos')
-    .then(async (res)=>{
-      const json = await res.json();
-      await setTodos(json)
-    })
+  const [value, setValue] = useState(0)
+  const [result, setresult] = useState(0)
+  // useEffect(() => {
+  //   fetch('http://localhost:3000/todos')
+  //   .then(async (res)=>{
+  //     const json = await res.json();
+  //     await setTodos(json)
+  //   })
     
-  }, [])
+  // }, [])
   
+  function calculate(){
+    let sum = 0
+    for(let i=1;i<=value;i++){
+      sum = sum+i
+      setresult(sum)
+      // console.log(sum)
+    }
+  }
 
-  console.log(todos)
-
+ 
   return (
     <>
-    <CreateTodo/>
-    <Todos todos={todos} />
+    <input value={value} onChange={(e)=>{setValue(e.target.value)}} type="number" />
+    <h2>sum of {result}</h2>
+    <button onClick={()=>calculate()}>click</button>
+    {/* <CreateTodo/>
+    <Todos todos={todos} /> */}
     </>
   )
 }
